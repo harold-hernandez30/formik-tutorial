094a2eca3a060df779a6d7643b6f028177f240d0
Harold Hernandez <harold.hernandez@visma.com>
2024-12-03 11:38:07 +0100

    Yup and useFormik().getFieldProps

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./styles.css";
-
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
-
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
-  return errors;
-};
 
 const SignupForm = () => {
   const formik = useFormik({
     initialValues: { firstName: "", lastName: "", email: "" },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Required email"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -41,9 +30,7 @@ const SignupForm = () => {
         id="firstName"
         name="firstName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
+        {...formik.getFieldProps("firstName")}
       />
 
       {formik.touched.firstName && formik.errors.firstName ? (
@@ -55,9 +42,7 @@ const SignupForm = () => {
         id="lastName"
         name="lastName"
         type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
+        {...formik.getFieldProps("lastName")}
       />
       {formik.touched.lastName && formik.errors.lastName ? (
         <div>{formik.errors.lastName}</div>
@@ -68,9 +53,7 @@ const SignupForm = () => {
         id="email"
         name="email"
         type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
+        {...formik.getFieldProps("email")}
       />
       {formik.touched.email && formik.errors.email ? (
         <div>{formik.errors.email}</div>
