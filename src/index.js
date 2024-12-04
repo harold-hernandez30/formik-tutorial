3d55f620cb6d9eba5bd00b0d549b9f8169483820
Harold Hernandez <harold.hernandez@visma.com>
2024-12-04 13:32:55 +0100

    withFormik implementation

--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useFormik } from "formik";
+import { useFormik, withFormik } from "formik";
 import * as Yup from "yup";
 import "./styles.css";
 
-const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: { firstName: "", lastName: "", email: "" },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      email: Yup.string()
-        .email("Invalid email address")
-        .required("Required email"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+const SignupForm = (props) => {
+  const { handleSubmit, getFieldProps, errors, touched } = props;
+
+  const submitIntercept = () => {
+    console.log("Submitting");
+    handleSubmit();
+  };
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={submitIntercept}>
       <label htmlFor="firstName">Last Name</label>
       <input
         id="firstName"
         name="firstName"
         type="text"
-        {...formik.getFieldProps("firstName")}
+        {...getFieldProps("firstName")}
       />
 
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
+      {touched.firstName && errors.firstName ? (
+        <div>{errors.firstName}</div>
       ) : null}
 
       <label htmlFor="lastName">First Name</label>
@@ -42,29 +31,44 @@ const SignupForm = () => {
         id="lastName"
         name="lastName"
         type="text"
-        {...formik.getFieldProps("lastName")}
+        {...getFieldProps("lastName")}
       />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
+      {touched.lastName && errors.lastName ? (
+        <div>{errors.lastName}</div>
       ) : null}
 
       <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        {...formik.getFieldProps("email")}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
+      <input id="email" name="email" type="email" {...getFieldProps("email")} />
+      {touched.email && errors.email ? <div>{errors.email}</div> : null}
       <button type="submit">Submit</button>
     </form>
   );
 };
 
+const FormikedSignupForm = withFormik({
+  initialValues: {
+    firstName: "harold",
+    lastName: "hernandez",
+    email: "harold@mail.com",
+  },
+  validationSchema: Yup.object({
+    firstName: Yup.string()
+      .max(15, "Must be 15 characters or less")
+      .required("Required"),
+    lastName: Yup.string()
+      .max(20, "Must be 20 characters or less")
+      .required("Required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Required email"),
+  }),
+  handleSubmit: (values, formikBag) => {
+    alert(JSON.stringify(values, null, 2));
+  },
+})(SignupForm);
+
 function App() {
-  return <SignupForm />;
+  return <FormikedSignupForm />;
 }
 
 const rootElement = document.getElementById("root");
